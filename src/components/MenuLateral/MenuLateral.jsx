1aa9807b1ad7b977de2da14e45351b6e581bb455
DiogoLCarvalho <diogo.carvalho.lc@gmail.com>
2024-05-13 21:45:38 -0300

    :sparkles: feature
    - add responsive code

--- src/components/MenuLateral/MenuLateral.jsx
+++ src/components/MenuLateral/MenuLateral.jsx
@@ -1,21 +1,32 @@
+import {useState} from "react";
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import "./style.css"
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { FaChartBar, FaProductHunt, FaIceCream  } from "react-icons/fa";
+import { MdOutlineStickyNote2, MdOutlineAccountBox, MdLogout,  } from "react-icons/md";
+import { TbCone2 } from "react-icons/tb";
+import { GiChocolateBar, GiStrawberry  } from "react-icons/gi";
+import { IoIosArrowForward } from "react-icons/io";
 
 const MenuLateral = ({ selecao, adminName }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <div className='menulateral__container'>
+      <div className='collapse__container' onClick={() => setCollapsed(e => !e)} style={{left: !collapsed ? "239px" : "70px"}}>
+        <IoIosArrowForward color='#fff'/>
+      </div>
       <Sidebar backgroundColor='#C3EFFF' rootStyles={{
         borderRight: "5px solid",
         borderRightColor: "#197CFF",
         height: "100%"
-      }}>
+      }} collapsed={collapsed}>
         <img src="../../public/imgs/logo_qgelado.svg" alt="Logo QGelado" className='logo__menulateral imagens-centralizadas' />
 
         <section className='adminName__container'>
-          <img src="../../public/imgs/MenuLateral/admin_photo.svg" alt="Foto perfil admin" className='photo_perfil imagens-centralizadas' />
-          <p style={{ fontSize: 20, fontWeight: "bold", textAlign: "center" }}>{adminName}</p>
+          <img src="../../public/imgs/Conta/admin_photo.svg" alt="Foto perfil admin" className='photo_perfil imagens-centralizadas' />
+          <p style={{ fontSize: 20, fontWeight: "bold", textAlign: "center", display: !collapsed ? "block" : "none" }}>{adminName}</p>
         </section>
 
         <Menu
@@ -29,16 +40,16 @@ const MenuLateral = ({ selecao, adminName }) => {
             },
           }}
         >
-          <MenuItem component={<Link to="/" />} active={selecao == "relatorio" ? true : false} >Relatório</MenuItem>
-          <SubMenu label="Cadastrar produto">
-            <MenuItem component={<Link to="/" />} active={selecao == "sorvetePadrao" ? true : false}>Sorvete padrão</MenuItem>
-            <MenuItem component={<Link to="/" />} active={selecao == "recipiente" ? true : false}>Recipiente</MenuItem>
-            <MenuItem component={<Link to="/" />} active={selecao == "acompanhamento" ? true : false}>Acompanhamento</MenuItem>
-            <MenuItem component={<Link to="/" />} active={selecao == "saborSorvete" ? true : false}>Sabor sorvete</MenuItem>
+          <MenuItem component={<Link to="/" />} active={selecao == "relatorio" ? true : false} icon={<FaChartBar />}>Relatório</MenuItem>
+          <SubMenu label="Cadastrar produto" icon={<FaProductHunt />}  >
+            <MenuItem component={<Link to="/" />} active={selecao == "sorvetePadrao" ? true : false} icon={<FaIceCream />}>Sorvete padrão</MenuItem>
+            <MenuItem component={<Link to="/" />} active={selecao == "recipiente" ? true : false} icon={<TbCone2 />}>Recipiente</MenuItem>
+            <MenuItem component={<Link to="/" />} active={selecao == "acompanhamento" ? true : false} icon={<GiChocolateBar />}>Acompanhamento</MenuItem>
+            <MenuItem component={<Link to="/" />} active={selecao == "saborSorvete" ? true : false}  icon={<GiStrawberry />}>Sabor sorvete</MenuItem>
           </SubMenu>
-          <MenuItem component={<Link to="/" />} active={selecao == "pedidos" ? true : false}>Pedidos</MenuItem>
-          <MenuItem component={<Link to="/" />} active={selecao == "conta" ? true : false}>Conta</MenuItem>
-          <MenuItem component={<Link to="/" />} active={selecao == "sair" ? true : false}>Sair</MenuItem>
+          <MenuItem component={<Link to="/" />} active={selecao == "pedidos" ? true : false}  icon={<MdOutlineStickyNote2 />}>Pedidos</MenuItem>
+          <MenuItem component={<Link to="/" />} active={selecao == "conta" ? true : false} icon={<MdOutlineAccountBox />}>Conta</MenuItem>
+          <MenuItem component={<Link to="/" />} active={selecao == "sair" ? true : false} icon={<MdLogout />}>Sair</MenuItem>
         </Menu>
       </Sidebar >
     </div>
--- src/pages/Conta/Conta.jsx
+++ src/pages/Conta/Conta.jsx
@@ -1,16 +1,40 @@
+import { useState } from 'react';
 import MenuLateral from '../../components/MenuLateral/MenuLateral';
 import './style.css';
 
+function PopupActions({title, textContent, redButtonText, greenButtonText, visibility, state}) {
+    return (
+        <section className={visibility ? 'conta__container__popup conta__container__popup--active' : 'conta__container__popup conta__container__popup--disable' }>
+            <div className='popup__window'> 
+                <div className='window__container__title'>
+                    <h2 className='title__text'>{title}</h2>
+                    <img src="../../public/imgs/Conta/close_popup.svg" alt="Ícone de fechar janela" className='title__icon' onClick={() => state(e => !e)}/>
+                </div>
+                <div className='window__container__textContent'>
+                    <p>{textContent}</p>
+                </div>
+                <div className='window__container__buttons'>
+                    <button className='botoes__botaoDeletar' onClick={() => state(e => !e)}>{redButtonText}</button>
+                    <button className='botoes__botaoSalvar'>{greenButtonText}</button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
 const Conta = () => {
+    const [visibilityAtualizar, setVisibilityAtualizar] = useState(false)
+    const [visibilityDeletar, setVisibilityDeletar] = useState(false)
+
     return (
         <main className='conta__main'>
-            <MenuLateral selecao="conta" adminName="Wilson Vendramel"/>
+            <MenuLateral selecao="conta" adminName="Wilson Vendramel" />
             <section className="container__conta">
                 <div className='conta__titulo'>
                     <h1>Conta</h1>
                 </div>
                 <div className='conta__nomeAdmin'>
-                    <img src="../../public/imgs/MenuLateral/admin_photo.svg" alt="Foto perfil admin" />
+                    <img src="../../public/imgs/Conta/admin_photo.svg" alt="Foto perfil admin" />
 
                     <p>Wilson Vendramel</p>
                 </div>
@@ -35,11 +59,30 @@ const Conta = () => {
                         </div>
                     </div>
                     <div className='informacoes__container__botoes'>
-                        <button className='botoes__botaoDeletar'>Deletar conta</button>
-                        <button className='botoes__botaoSalvar'>Salvar</button>
+                        <button className='botoes__botaoDeletar' onClick={() => setVisibilityDeletar(e => !e)}>Deletar conta</button>
+                        <button className='botoes__botaoSalvar' onClick={() => setVisibilityAtualizar(e => !e)}>Salvar</button>
                     </div>
                 </div>
             </section>
+
+            <PopupActions 
+                title="Deletar Conta" 
+                textContent="Tem certeza que deseja deletar sua conta? Essa ação não pode ser desfeita" 
+                redButtonText="Cancelar" 
+                greenButtonText="Deletar"
+                visibility={visibilityDeletar}
+                state={setVisibilityDeletar}
+            />
+
+            <PopupActions 
+                title="Atualizar conta" 
+                textContent="Tem certeza que deseja atualizar suas informações?" 
+                redButtonText="Cancelar" 
+                greenButtonText="Atualizar" 
+                visibility={visibilityAtualizar}
+                state={setVisibilityAtualizar}
+            />
+
         </main>
     )
 }
