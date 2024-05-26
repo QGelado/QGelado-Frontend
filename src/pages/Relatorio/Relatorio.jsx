34bd1ed8958464f3b7256340f12daf678eb74e77
DiogoLCarvalho <diogo.carvalho.lc@gmail.com>
2024-05-26 19:12:35 -0300

    :sparkles: finish report

--- src/pages/Relatorio/Relatorio.jsx
+++ src/pages/Relatorio/Relatorio.jsx
@@ -2,23 +2,119 @@ import { useState, useEffect } from 'react'
 import './style.css'
 import MenuLateral from '../../components/MenuLateral/MenuLateral';
 import Axios from 'axios';
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import data from './dataReport';
+
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTY3NTgwOTQsImV4cCI6MTcxNjg0NDQ5NH0.-irUs4WVd_Z8mCkcX-w2m_tG81O1sUV7RinRwy77SRs";
 
 const Relatorio = () => {
     const [qtdUsuarios, setQtdUsuarios] = useState(0);
+    const [valorTotalSorvetes, setValorTotalSorvetes] = useState(0);
+    const [valorTotalVendas, setValorTotalVendas] = useState(0);
+    const [nomesSorvetesMaisVendidos, setNomeSorvetesMaisVendidos] = useState([]);
+    const [nomesUsuariosVendidos, setNomesUsuariosVendidos] = useState([]);
+    const [qtdSorvetesmensais, setQtdSorvetesMensais] = useState([]);
+
+    useEffect(() => {
+        Axios.get(`http://localhost:3000/pedidos/valor-por-meses`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        })
+            .then((response) => {
+                const dataComMeses = data;
+                let objOriginal = response.data;
+                dataComMeses.forEach((j, i) => {
+                    objOriginal.forEach(e => {
+                        if (i + 1 == e._id.month) {
+                            j.valor = e.totalValor.$numberDecimal
+                        }
+                    })
+                })
+                console.log(dataComMeses)
+
+                setQtdSorvetesMensais(dataComMeses)
+            })
+            .catch((error) => {
+                console.log(error)
+                setQtdSorvetesMensais([])
+            })
+    }, [])
+
+    useEffect(() => {
+        Axios.get(`http://localhost:3000/pedidos/sorvetes`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        })
+            .then((response) => {
+                setNomeSorvetesMaisVendidos(response.data.filter((e, i) => i <= 3))
+            })
+            .catch((error) => {
+                console.log(error)
+                setNomeSorvetesMaisVendidos(0)
+            })
+    }, [])
+
+    useEffect(() => {
+        Axios.get(`http://localhost:3000/pedidos/valor-total`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        })
+            .then((response) => {
+                setValorTotalVendas(parseFloat(response.data[0].totalValor.$numberDecimal))
+            })
+            .catch((error) => {
+                console.log(error)
+                setValorTotalVendas(0)
+            })
+    }, [])
+
+    useEffect(() => {
+        Axios.get(`http://localhost:3000/pedidos/sorvetes-valor-total`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        })
+            .then((response) => {
+                setValorTotalSorvetes(parseFloat(response.data[0].totalValor.$numberDecimal))
+            })
+            .catch((error) => {
+                console.log(error)
+                setValorTotalSorvetes(0)
+            })
+    }, [])
+
+    useEffect(() => {
+        Axios.get(`http://localhost:3000/pedidos/usuarios`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        })
+            .then((response) => {
+                setNomesUsuariosVendidos(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+                setNomesUsuariosVendidos([])
+            })
+    }, [])
 
     useEffect(() => {
         Axios.get(`http://localhost:3000/usuario/quantidadeCadastrados`, {
             headers: {
-                Authorization: `Bearer`,
+                Authorization: `Bearer ${token}`,
             }
         })
-        .then((response) => {
-            setQtdUsuarios(response.data)
-        })
-        .catch((error) => {
-            console.log(error)
-            setQtdUsuarios(0)
-        })
+            .then((response) => {
+                setQtdUsuarios(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+                setQtdUsuarios(0)
+            })
     }, [])
 
     return (
@@ -28,34 +124,77 @@ const Relatorio = () => {
             <section className='container__relatorio'>
                 <section className='relatorio__vendasMensais__container'>
                     <h2 className='relatorio__title__section'>Alcance mensal de vendas</h2>
-                    <div className='relatorio__backgroundData relatorio__backgroundData--big'></div>
+                    <div className='relatorio__backgroundData relatorio__backgroundData--big'>
+                        {qtdSorvetesmensais.length != 0 ? (
+                            <ResponsiveContainer width="100%" height="100%">
+                                <LineChart
+                                    width={500}
+                                    height={300}
+                                    data={qtdSorvetesmensais}
+                                    margin={{
+                                        top: 5,
+                                        right: 30,
+                                        left: 20,
+                                        bottom: 5,
+                                    }}
+                                >
+                                    <CartesianGrid strokeDasharray="3 3" />
+                                    <XAxis dataKey="mes" />
+                                    <YAxis />
+                                    <Tooltip />
+                                    <Legend />
+                                    <Line type="monotone" dataKey="valor" stroke="#FF90C8" activeDot={{ r: 8 }} />
+                                </LineChart>
+                            </ResponsiveContainer>
+                        ) : null}
+                    </div>
                 </section>
 
                 <section className='relatorio__sorvetesMaisVendidos__container'>
                     <h2 className='relatorio__title__section'>Sorvetes mais vendidos</h2>
-                    <div className='relatorio__backgroundData relatorio__backgroundData--big'></div>
+                    <div className='relatorio__backgroundData relatorio__backgroundData--big  relatorio__backgroundData--block'>
+                        {nomesSorvetesMaisVendidos.map((e, i) => (
+                            <div key={i} style={{ width: "100%", display: "flex", justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                <p style={{ textAlign: 'left', width: "250px" }} className='relatorio__backgroundData__text--medium'>{e.nome}</p>
+                                <p className='relatorio__backgroundData__text--medium'>{e.count}</p>
+                            </div>
+                        )
+                        )}
+                    </div>
                 </section>
 
                 <section className='relatorio__valorTotalVendas__container'>
                     <h2 className='relatorio__title__section'>Valor total de vendas</h2>
-                    <div className='relatorio__backgroundData relatorio__backgroundData--medium'></div>
+                    <div className='relatorio__backgroundData relatorio__backgroundData--medium'>
+                        <p className='relatorio__backgroundData__text--bold'>R$ {valorTotalVendas}</p>
+                    </div>
                 </section>
 
                 <section className='relatorio__vendasTotalSorvetes__container'>
                     <h2 className='relatorio__title__section'>Valor total sorvetes</h2>
-                    <div className='relatorio__backgroundData relatorio__backgroundData--medium'></div>
+                    <div className='relatorio__backgroundData relatorio__backgroundData--medium'>
+                        <p className='relatorio__backgroundData__text--bold'>R$ {valorTotalSorvetes}</p>
+                    </div>
                 </section>
 
                 <section className='relatorio__usuariosComMaisVendas__container'>
                     <h2 className='relatorio__title__section'>Usuários com mais vendas</h2>
-                    <div className='relatorio__backgroundData relatorio__backgroundData--big'></div>
+                    <div className='relatorio__backgroundData relatorio__backgroundData--big relatorio__backgroundData--block'>
+                        {nomesUsuariosVendidos.map((e, i) => (
+                            <div key={i} style={{ width: "100%", display: "flex", justifyContent: 'space-evenly', alignItems: 'center' }}>
+                                <p style={{ textAlign: 'left', width: "250px" }} className='relatorio__backgroundData__text--medium'>{e.nome}</p>
+                                <p className='relatorio__backgroundData__text--medium'>{e.count}</p>
+                            </div>
+                        )
+                        )}
+                    </div>
                 </section>
 
                 <section className='relatorio__numeroVisitas__relatorio'>
                     <h2 className='relatorio__title__section'>Numero de visitas</h2>
                     <div className='relatorio__backgroundData relatorio__backgroundData--big'>
-                        <p>Usuários cadastrados</p>
-                        <p>{qtdUsuarios}</p>
+                        <p className='relatorio__backgroundData__text--bold'>Usuários cadastrados</p>
+                        <p className='relatorio__backgroundData__text--bold'>{qtdUsuarios}</p>
                     </div>
                 </section>
             </section>
---
+++ src/pages/Relatorio/dataReport.js
@@ -0,0 +1,52 @@
+const data = [
+    {
+        mes: 'Jan',
+        valor: 0
+    },
+    {
+        mes: 'Fev',
+        valor: 0
+    }, 
+    {
+        mes: 'Mar',
+        valor: 0
+    },
+    {
+        mes: 'Abr',
+        valor: 0
+    },
+    {
+        mes: 'Mai',
+        valor: 0
+    },
+    {
+        mes: 'Jun',
+        valor: 0
+    },
+    {
+        mes: 'Jul',
+        valor: 0
+    },
+    {
+        mes: 'Ago',
+        valor: 0
+    },
+    {
+        mes: 'Set',
+        valor: 0
+    },
+    {
+        mes: 'Out',
+        valor: 0
+    },
+    {
+        mes: 'Nov',
+        valor: 0
+    },
+    {
+        mes: 'Dez',
+        valor: 0
+    },
+];
+
+export default data;
