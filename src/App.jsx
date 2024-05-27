d7dec61ab8eaf8445ad6eb0efd703b0d2e3d0739
DiogoLCarvalho <diogo.carvalho.lc@gmail.com>
2024-05-27 11:25:44 -0300

    :sparkles: feature
    - finish 'Pedidos' page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home/Home'
 import Conta from './pages/Conta/Conta'
 import Estoque from './pages/Estoque/Estoque'
+import Pedidos from './pages/Pedidos/Pedidos'
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" element={<Home/>}/>
         <Route path="/conta" element={<Conta/>}/>
         <Route path="/estoque" element={<Estoque/>}/>
+        <Route path="/pedidos" element={<Pedidos/>}/>
       </Routes>
     </BrowserRouter>
   )
