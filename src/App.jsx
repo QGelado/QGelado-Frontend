7247c9579d1b5bbec4791ffbd3ab58565bff6108
AdemirMA <ademirasahikawa@outlook.com>
2024-05-28 19:36:44 -0300

    Login, Cadastro, Perfil

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home/Home'
 import Conta from './pages/Conta/Conta'
 import './App.css';
+import Perfil from "./pages/Perfil/Perfil";
+import Login from "./pages/Login/Login";
+import Cadastro from "./pages/Cadastro/Cadastro";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/" element={<Login/>}/>
+        <Route exact path="/cadastrar" element={<Cadastro/>}/>
+        <Route exact path="/home" element={<Home/>}/>
         <Route path="/conta" element={<Conta/>}/>
+        <Route path="/perfil" element={<Perfil/>} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/config/environment.js
@@ -0,0 +1,5 @@
+export const api = {
+  baseURL: import.meta.env.VITE_APP_BASE_API ,
+};
+
+export const jwtKey = import.meta.env.VITE_APP_JWT_KEY;
