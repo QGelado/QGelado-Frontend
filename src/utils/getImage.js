1307204e1e9d4e898248dfe239667eadf6afe8c6
Aryel Anne Louise de Souza Gabriel <aannelouise@outlook.com>
2024-06-16 18:16:46 -0300

    Imagens

--- src/utils/getImage.js
+++ src/utils/getImage.js
@@ -1,6 +1,6 @@
 export function getImage(img){
     if(typeof img === 'string' && (img.startsWith('/sorvete') || img.startsWith('/sabor') || img.startsWith('/recipiente') || img.startsWith('/acompanhamento'))){
-        return import.meta.env.VITE_APP_BASE_API + img
+        return import.meta.env.VITE_APP_BASE_API.slice(0, -1) + img
     }
     else if(typeof img === 'string' && img?.startsWith('/src')){
         return img
